f8d18135e49da48b63e1623415a7763f6a755e9f
James <james_baster@shelter.org.uk>
2015-11-17 13:56:37 +0000

    Another test for data text file loading

---
+++ tests/DataLoadText1Test/DataLoadText1Test.php
@@ -0,0 +1,61 @@
+<?php
+
+
+use staticsquish\filters\RootDataObjectFilter;
+use staticsquish\aggregation\DistinctValuesAggregation;
+
+/**
+*  @license 3-clause BSD
+*/
+class DataLoadText1Test extends PHPUnit_Framework_TestCase {
+
+  public function testData1() {
+
+    global $app;
+
+    $site = new \staticsquish\Site($app, __DIR__.DIRECTORY_SEPARATOR.'siteData1');
+
+    $this->assertEquals(0, count($site->getWarnings()));
+    $this->assertEquals(0, count($site->getErrors()));
+
+    $rootDataObjects = $site->getRootDataObjects();
+    $this->assertEquals(2, count($rootDataObjects));
+
+    // FIRST OBJECT
+    $rootDataObject = $rootDataObjects[0];
+
+    $this->assertEquals('test1', $rootDataObject->getTitle());
+    $this->assertEquals('test1', $rootDataObject->getSlug());
+
+    $fields = $rootDataObject->getFields();
+    $this->assertNotNull($fields['colour']);
+    $this->assertEquals('staticsquish\models\FieldValue', get_class( $fields['colour']));
+    $this->assertTrue($fields['colour']->hasValue());
+    $this->assertEquals('red', $fields['colour']->getValue());
+
+    $fields = $rootDataObject->getFields();
+    $this->assertNotNull($fields['shape']);
+    $this->assertEquals('staticsquish\models\FieldValue', get_class( $fields['shape']));
+    $this->assertTrue($fields['shape']->hasValue());
+    $this->assertEquals('box', $fields['shape']->getValue());
+
+
+
+    // SECOND OBJECT
+    $rootDataObject = $rootDataObjects[1];
+
+    $this->assertEquals('test2', $rootDataObject->getTitle());
+    $this->assertEquals('test2', $rootDataObject->getSlug());
+
+
+
+    $fields = $rootDataObject->getFields();
+    $this->assertNotNull($fields['shape']);
+    $this->assertEquals('staticsquish\models\FieldValue', get_class( $fields['shape']));
+    $this->assertTrue($fields['shape']->hasValue());
+    $this->assertEquals('square', $fields['shape']->getValue());
+
+  }
+
+
+}
