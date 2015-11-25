c756ae9f9ecdcdb3bd30cf7d4062d8352525ee1b
James <james_baster@shelter.org.uk>
2015-11-25 11:10:33 +0000

    Add passing test for normal date time loading

---
+++ tests/DataLoadDateTime1Test/DataLoadDateTime1Test.php
@@ -0,0 +1,50 @@
+<?php
+
+use staticsquish\FileLoaderTxt;
+use staticsquish\config\FieldConfig;
+
+/**
+*  @license 3-clause BSD
+*/
+class DataLoadDateTime1Test extends PHPUnit_Framework_TestCase {
+
+
+    function test1() {
+
+
+        global $app;
+
+        $site = new \staticsquish\Site($app, __DIR__.DIRECTORY_SEPARATOR.'siteData1');
+
+        $this->assertEquals(0, count($site->getWarnings()));
+        $this->assertEquals(0, count($site->getErrors()));
+
+        $rootDataObjects = $site->getRootDataObjects();
+        $this->assertEquals(2, count($rootDataObjects));
+
+        // FIRST OBJECT
+        $rootDataObject = $rootDataObjects[0];
+
+        $this->assertEquals('test1', $rootDataObject->getTitle());
+        $this->assertEquals('test1', $rootDataObject->getSlug());
+
+        $fields = $rootDataObject->getFields();
+
+        $this->assertNotNull($fields['started']);
+        $this->assertEquals('staticsquish\models\FieldScalarValueDateTime', get_class( $fields['started']));
+        $this->assertEquals('2015-02-02T10:01:23+00:00', $fields['started']->getValueAsString() );
+
+        // SECOND OBJECT
+        $rootDataObject = $rootDataObjects[1];
+
+        $this->assertEquals('test2', $rootDataObject->getTitle());
+        $this->assertEquals('test2', $rootDataObject->getSlug());
+
+        $fields = $rootDataObject->getFields();
+
+        $this->assertNotNull($fields['started']);
+        $this->assertEquals('staticsquish\models\FieldScalarValueDateTime', get_class( $fields['started']));
+        $this->assertEquals('2015-07-04T10:01:23+01:00', $fields['started']->getValueAsString() );
+
+    }
+}
